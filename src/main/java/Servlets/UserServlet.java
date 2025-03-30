f1778cce752d7a109c1e848b78bd614a49d67b7b
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-03-30 19:56:54 +0300

    Initial commit

---
+++ src/main/java/Servlets/UserServlet.java
@@ -0,0 +1,41 @@
+package Servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+
+        if (username == null) {
+            username = "DefaultUser"; // Якщо користувач вперше зайшов
+            session.setAttribute("username", username);
+        }
+        else
+            username = "OK";
+
+        out.println("<h1>Welcome, " + username + "!</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.println("{\"message\": \"User saved in session\", \"username\": \"" + username + "\"}");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.io.PrintWriter" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+
+</body>
+</html>
