25d3262f83604627cb1afcb66141f7feb208643c
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-04-04 21:38:51 +0300

    update

--- src/main/java/Database/DataSourceManager.java
+++ src/main/java/Database/DataSourceManager.java
@@ -1,6 +1,19 @@
 package Database;
 import javax.sql.DataSource;
-//import org.apacche.commons
+import org.apache.commons.dbcp2.BasicDataSource;
 public class DataSourceManager {
-    //private static final BasicDataSource
+    private static final BasicDataSource  dataSource = new BasicDataSource();
+
+    static {
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb");
+        dataSource.setUsername("Rostyslav");
+        dataSource.setPassword("5720693");
+        dataSource.setMinIdle(5);
+        dataSource.setMaxIdle(10);
+        dataSource.setMaxOpenPreparedStatements(100);
+    }
+
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
 }
